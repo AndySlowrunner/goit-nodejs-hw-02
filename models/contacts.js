46439a610d2f020c98fa50704f071e78b2c6ae70
Andy <andyfartushniy@gmail.com>
2023-12-13 10:11:10 +0200

    Test

--- models/contacts.js
+++ models/contacts.js
@@ -1,6 +1,8 @@
 // const fs = require('fs/promises')
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  console.log('Hello Node.js!')
+}
 
 const getContactById = async (contactId) => {}
 
